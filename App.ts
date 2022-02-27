9500b84e713816972373655775a59b353ecb20e6
Adam Knieć <adam.kniec@netguru.com>
2022-02-27 17:05:21 +0100

    adding the connection to mongodb

--- App.ts
+++ App.ts
@@ -1,13 +1,23 @@
 export {};
-const express = require("express");
 
+const MongoClient = require("mongodb").MongoClient;
+const express = require("express");
+const dotenv = require("dotenv");
 const expensesRoutes = require("./routes");
+
+dotenv.config();
 
 const app = express();
 const port = 5001;
+const pass = process.env.MONGO_USER_PASSWORD;
 
 app.use("/", expensesRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is up and running on port ${port}`);
-});
+MongoClient.connect(
+  `mongodb+srv://adas:${pass}@xpenses.lxcl9.mongodb.net/xpenses?retryWrites=true&w=majority`
+)
+  .then(
+    () => console.log("Succesfully connected to database"),
+    app.listen(port)
+  )
+  .catch((err: any) => console.log(err)); // TODO -> Get rid of any
